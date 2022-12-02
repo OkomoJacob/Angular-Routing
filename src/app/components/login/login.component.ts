d1a9d36afad4ae4568aa9ffa40b23a3d387eaff0
okomojacob <okomo.code@gmail.com>
2022-12-02 12:25:30 +0300

    Initial login page :lock: :pencil2:

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,23 @@
+import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { faLock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  faLock = faLock;
+  loginForm = new FormGroup({
+    email: new FormControl(''),
+    password: new FormControl(''),
+  });
+  constructor( private router: Router) {}
 
   ngOnInit(): void {
   }
-
+  onSubmit(): void {
+  }
 }
