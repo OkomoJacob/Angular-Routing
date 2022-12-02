d29d40e3fa223191ce6bc221946e6289e7778fe4
okomojacob <okomo.code@gmail.com>
2022-12-02 11:51:21 +0300

    Initial :sparkles: set-up :tada:

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { LoginComponent } from './components/login/login.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'forgot-password', component: ForgotPasswordComponent},
+  { path: '**', component: NotFoundComponent},
+  { path: '', redirectTo: '/login', pathMatch: 'full'}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
