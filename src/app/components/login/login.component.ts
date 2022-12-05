7e2e416a9876ad84b322eaffa038226ef29b9258
okomojacob <okomo.code@gmail.com>
2022-12-05 11:31:28 +0300

    Login updates :lock:

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { faLock } from '@fortawesome/free-solid-svg-icons';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -14,10 +15,26 @@ export class LoginComponent implements OnInit {
     email: new FormControl(''),
     password: new FormControl(''),
   });
-  constructor( private router: Router) {}
+  constructor( private router: Router, private auth: AuthService) {}
 
   ngOnInit(): void {
+    if(this.auth.isLoggedIn()) {
+      this.router.navigate(['admin']);
+    }
   }
+  
   onSubmit(): void {
+    console.log(this.loginForm.value);
+    if(this.loginForm.valid){
+      this.auth.login(this.loginForm.value).subscribe({
+        next: (response: any) => {
+          this.router.navigate(['admin']);
+        },
+        error: (error) => {
+          alert(error.message);
+        }
+      }
+      );
+    }
   }
 }
