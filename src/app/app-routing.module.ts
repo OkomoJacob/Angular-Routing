1dc17da7c43e53b488206f335d91008d10fad058
okomojacob <okomo.code@gmail.com>
2022-12-05 11:28:06 +0300

    Gurding admin routes :lock:

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './guards/auth.guard';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { LoginComponent } from './components/login/login.component';
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'admin', 
+    canActivate: [AuthGuard],
     loadChildren: () => import('./modules/admin/admin.module')
     .then((m) => m.AdminModule), 
   },
